80f1af53144aef3697b30ffa720c168d6f1793cb
Hamis BADAROU <hbadarou@ext.sephora.fr>
2022-07-10 16:09:24 +0000

    sephora_pipeline_20220710

--- models/staging/funnel/global/stg_funnel_eme_reporting.sql
+++ models/staging/funnel/global/stg_funnel_eme_reporting.sql
@@ -8,72 +8,7 @@
 with
     funnel_data as (
 
-            select 
-            date,
-            Country_ as country,
-            regie_source,
-            campaign,
-            media_type,
-            case
-                when
-                    regie_source in (
-                        'facebookads', 'snapchat'
-                    ) and lower(campaign) like '%_ec_%' and lower(campaign) not like '%ecom%'
-                then 'PERF'
-                when regie_source = 'facebook' and campaign like '%_CS_TRAF_%' and lower(campaign) not like '%coad%'
-                then 'PERF'
-                when
-                    regie_source = 'adwords'
-                    and media_type = 'Display'
-                    and campaign not like '%_EC_%'
-                then 'OTHERS'
-                when
-                    regie_source = 'adwords'
-                    /*--and media_type = 'Display'*/
-                    and lower(campaign) like '%_ec_%'
-                then 'PERF'
-                when regie_source in ('criteo') and lower(campaign) not like '%trademarketing%' then 'PERF'  
-                when regie_source in ('rtbhouse', 'awin', 'tiktok')                
-                then 'PERF'
-                else 'OTHERS'
-             end as campaign_type,
-        
-            case
-                when regie_source in ('facebookads', 'tiktok', 'snapchat')
-                then 'Social'
-                when regie_source in ('rtbhouse', 'criteo')
-                then 'Retargeting'
-                when Country_ ='SCANDI' and lower(campaign) like '%_bra_%' then 'Paid Search Brand'
-                when regie_source in ('awin')
-                then 'Affiliation'
-                when
-                    regie_source = 'adwords'
-                    and lower(campaign) like '%brandsephora%'
-                    or lower(campaign) like '%brand sephora%'
-                    or lower(campaign) like '%brandbidding%'
-                    or lower(campaign) like '%core-brand%'
-                    or lower(campaign) like '%bra-exact%'
-                    or lower(campaign) like '%sephora brand%'
-                    or lower(campaign) like '%exclusivebrand%'
-                then 'Paid Search Brand'
-                /*
-                when
-                    regie_source = 'adwords'
-                    and campaign not like '%BrandSephora%'
-                    or campaign not like '%Brand Sephora%'
-                    or campaign not like '%BrandBidding%'
-                    or campaign not like '%Core-Brand%'
-                    or campaign not like '%BRA-Exact%'
-                    or campaign not like 'sephora brand'
-                then 'Paid Search Non Brand'
-                */
-                else 'Paid Search Non Brand'
-            end as channel_grouping,
-            cost,
-            impressions,
-            clicks, 
-            revenue
-        from {{ ref('stg_funnel_global_report') }}
+       select * from {{ ref('stg_funnel_country_perf') }}
     )
 
 select
